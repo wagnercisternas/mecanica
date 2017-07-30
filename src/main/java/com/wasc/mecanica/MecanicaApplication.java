3054aa5c6ccd777a3a227d9de911866cad8a7a1d
wagnersalamanca@gmail.com <wagnersalamanca@gmail.com>
2017-07-30 19:51:45 -0300

    add 1st commit

---
+++ src/main/java/com/wasc/mecanica/MecanicaApplication.java
@@ -0,0 +1,12 @@
+package com.wasc.mecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecanicaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wasc/mecanica/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wasc.mecanica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MecanicaApplication.class);
+	}
+
+}
