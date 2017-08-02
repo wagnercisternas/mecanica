80f373e3dfd7593a11d0211dc7eb60345ce31331
wagnersalamanca@gmail.com <wagnersalamanca@gmail.com>
2017-08-02 15:22:18 -0300

    iniciando bean validation

--- src/main/java/com/wasc/mecanica/controller/ClienteController.java
+++ src/main/java/com/wasc/mecanica/controller/ClienteController.java
@@ -1,7 +1,11 @@
 package com.wasc.mecanica.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,17 +21,32 @@ public class ClienteController {
 	private Clientes clientes;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroCliente";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroCliente");
+		mv.addObject(new Cliente());
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST )
-	public ModelAndView salvar(Cliente cliente) {
+	public ModelAndView salvar(@Validated Cliente cliente, Errors errors) {
+		ModelAndView mv = new ModelAndView("CadastroCliente");
+
+		if (errors.hasErrors()) {
+			return mv;
+		}
 		
 		clientes.save(cliente);
 		
-		ModelAndView mv = new ModelAndView("CadastroCliente");
 		mv.addObject("mensagem", "Cliente salvo com sucesso!");
+		return mv;
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar() {
+		List<Cliente> todosClientes = clientes.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaCliente");
+		mv.addObject("clientes",todosClientes);
+		
 		return mv;
 	}
 }
--- src/main/java/com/wasc/mecanica/model/Cliente.java
+++ src/main/java/com/wasc/mecanica/model/Cliente.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Cliente {
@@ -12,10 +15,14 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotEmpty(message = "Informar nome do cliente")
 	private String nomeCliente;
 	
+	@NotNull(message = "Informar CPF cliente")
 	private Long cpfCliente;
 	
+	
+	@NotNull(message = "Informar tel contato")
 	private Long celCliente;
 
 	@Override
